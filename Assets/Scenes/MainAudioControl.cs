b04fb0d791cf8342da3bc348483536243e5c4f96
Taeyang <kt0609@outlook.com>
2024-05-03 15:55:21 +0900

    오디오 볼륨 조절 슬라이더 정상 작동
    - Option 내의 슬라이더를 이용해 볼륨 조절이 가능함.
    - 슬라이더 1개로 모든 오디오 볼륨을 한 번에 조절할 수 있음

---
+++ Assets/Scenes/MainAudioControl.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class MainAudioControl : MonoBehaviour
+{
+    public AudioMixer masterMixer;
+    public Slider audioSlider;
+    public void AudioControl()
+    {
+        float sound = audioSlider.value;
+
+        if (sound == -40f)
+        {
+            masterMixer.SetFloat("AudioMaster", -80);
+        }
+        else
+        {
+            masterMixer.SetFloat("AudioMaster", sound);
+        }
+    }
+}
