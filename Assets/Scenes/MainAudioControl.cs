65176f833f8aa72bcc9e8cd37457eab9fcb2c866
Ryu Taeyang <kt0609@outlook.com>
2024-09-29 04:06:51 +0900

    볼륨 조절 슬라이더
    Main의 오디오 조절 슬라이더와 Play의 오디오 조절 슬라이더가 같은 값을 사용할 수 있도록 연결

--- Assets/Scenes/MainAudioControl.cs
+++ Assets/Scenes/MainAudioControl.cs
@@ -9,10 +9,27 @@ public class MainAudioControl : MonoBehaviour
     public AudioMixer masterMixer;
     public Slider audioSlider;
 
+    void Start()
+    {
+        float currentVolume = PlayerPrefs.GetFloat("AudioMaster", -20f);
+        audioSlider.value = currentVolume;
+
+    }
+
     public void AudioControl()
     {
         float sound = audioSlider.value;
 
+        PlayerPrefs.SetFloat("AudioMaster", sound);
+        PlayerPrefs.Save();
+
+        SetVolume(sound);
+
+    }
+
+    private void SetVolume(float sound)
+    {
+        // 볼륨 조절 슬라이더의 값이 -40f 이하인 경우 음소거와 같은 것으로 처리
         if (sound == -40f)
         {
             masterMixer.SetFloat("AudioMaster", -80);
